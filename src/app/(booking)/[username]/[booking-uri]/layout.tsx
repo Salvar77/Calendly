ce1cab58e778a7b0732673000c743dea0b3a0230
Łukasz <lukaszkus77@gmail.com>
2025-04-02 22:00:19 +0200

    initial commit

--- src/app/(booking)/[username]/[booking-uri]/layout.tsx
+++ src/app/(booking)/[username]/[booking-uri]/layout.tsx
@@ -1,41 +1,12 @@
-import { Clock, Info } from "lucide-react";
-import mongoose from "mongoose";
-import { ProfileModel } from "@/models/Profiles";
-import { EventTypeModel } from "@/models/EventType";
-import { notFound } from "next/navigation";
+// src/app/(booking)/[username]/[booking-uri]/layout.tsx
+
 import { ReactNode } from "react";
 
 type LayoutProps = {
   children: ReactNode;
-  params: {
-    username: string;
-    "booking-uri": string;
-  };
 };
 
-export default async function BookingBoxLayout({
-  children,
-  params,
-}: LayoutProps) {
-  const username = decodeURIComponent(params.username);
-  const bookingUri = decodeURIComponent(params["booking-uri"]);
-
-  await mongoose.connect(process.env.MONGODB_URI as string);
-
-  const profileDoc = await ProfileModel.findOne({ username });
-  if (!profileDoc) {
-    return notFound();
-  }
-
-  const eventType = await EventTypeModel.findOne({
-    email: profileDoc.email,
-    uri: bookingUri,
-  });
-
-  if (!eventType) {
-    return notFound();
-  }
-
+export default function BookingBoxLayout({ children }: LayoutProps) {
   return (
     <div
       className="flex items-center justify-center h-screen bg-cover"
@@ -43,24 +14,6 @@ export default async function BookingBoxLayout({
     >
       <div className="w-full text-center">
         <div className="inline-flex mx-auto shadow-md rounded-lg overflow-hidden">
-          <div className="bg-blue-100/50 p-4 w-80 text-gray-800">
-            <h1 className="text-left text-2xl font-bold mb-4 pb-2 border-b border-black/10">
-              {eventType.title}
-            </h1>
-
-            <div className="grid gap-y-4 grid-cols-[40px_1fr] text-left">
-              <div className="flex justify-center">
-                <Clock />
-              </div>
-              <div>{eventType.length} min</div>
-
-              <div className="flex justify-center">
-                <Info />
-              </div>
-              <div>{eventType.description}</div>
-            </div>
-          </div>
-
           <div className="bg-white/80 grow p-8">{children}</div>
         </div>
       </div>
--- src/app/(booking)/[username]/[booking-uri]/page.tsx
+++ src/app/(booking)/[username]/[booking-uri]/page.tsx
@@ -3,6 +3,7 @@ import { EventTypeModel } from "@/models/EventType";
 import { ProfileModel } from "@/models/Profiles";
 import mongoose from "mongoose";
 import { notFound } from "next/navigation";
+import { Clock, Info } from "lucide-react";
 
 type PageProps = {
   params: {
@@ -12,14 +13,13 @@ type PageProps = {
   searchParams: Record<string, string>;
 };
 
-export default async function BookingPage(props: PageProps) {
+export default async function BookingPage({ params }: PageProps) {
   await mongoose.connect(process.env.MONGODB_URI as string);
 
-  const username = decodeURIComponent(props.params.username);
-  const bookingUri = decodeURIComponent(props.params["booking-uri"]);
+  const username = decodeURIComponent(params.username);
+  const bookingUri = decodeURIComponent(params["booking-uri"]);
 
   const profileDoc = await ProfileModel.findOne({ username });
-
   if (!profileDoc) {
     return notFound();
   }
@@ -33,14 +33,34 @@ export default async function BookingPage(props: PageProps) {
     return notFound();
   }
 
-  const { uri, length, bookingTimes } = foundEvent;
+  const { uri, length, bookingTimes, title, description } = foundEvent;
 
   return (
-    <TimePicker
-      username={username}
-      meetingUri={uri}
-      length={length}
-      bookingTimes={JSON.parse(JSON.stringify(bookingTimes))}
-    />
+    <div className="flex flex-col items-center w-full">
+      <div className="bg-blue-100/50 p-4 w-80 text-gray-800 mb-6 rounded shadow">
+        <h1 className="text-left text-2xl font-bold mb-4 pb-2 border-b border-black/10">
+          {title}
+        </h1>
+
+        <div className="grid gap-y-4 grid-cols-[40px_1fr] text-left">
+          <div className="flex justify-center">
+            <Clock />
+          </div>
+          <div>{length} min</div>
+
+          <div className="flex justify-center">
+            <Info />
+          </div>
+          <div>{description}</div>
+        </div>
+      </div>
+
+      <TimePicker
+        username={username}
+        meetingUri={uri}
+        length={length}
+        bookingTimes={JSON.parse(JSON.stringify(bookingTimes))}
+      />
+    </div>
   );
 }
